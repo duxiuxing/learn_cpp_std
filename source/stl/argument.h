c09ea1d212aef8d15a8622fad462943a227baa20
杜修杏 <duxiuxing@foxmail.com>
2018-11-25 14:08:07 +0800

    精简代码

--- source/stl/argument.h
+++ source/stl/argument.h
@@ -8,65 +8,54 @@ class ArgumentT
 {
 public:
     typedef typename StringType::value_type CharType;
-    typedef std::vector<ArgumentT<StringType>*> List;
+    typedef ArgumentT<StringType> ArgumentType;
+    typedef std::vector<ArgumentType> List;
 
     ArgumentT(List& argList, int index, const CharType* text)
         : m_argList(argList), m_index(index), m_text(text)
     {}
 
-    ArgumentT<StringType>* Next();
-    ArgumentT<StringType>* Previous();
+    ArgumentType* Next()
+    {
+        int nextIndex = m_index + 1;
+        if (nextIndex > 0 && nextIndex < static_cast<int>(m_argList.size()))
+        {
+            return &m_argList[nextIndex];
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
 
-    const CharType* Text();
-    bool IsEqualTo(const CharType* text);
+    ArgumentType* Previous()
+    {
+        int prevIndex = m_index - 1;
+        if (prevIndex > 0 && prevIndex < static_cast<int>(m_argList.size()))
+        {
+            return &m_argList[prevIndex];
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+
+    const CharType* Text()
+    {
+        return m_text.c_str();
+    }
+
+    bool IsEqualTo(const CharType* text)
+    {
+        return (0 == m_text.compare(text));
+    }
 
 protected:
     List& m_argList;
     int m_index;
     StringType m_text;
 };
-
-template<class StringType>
-ArgumentT<StringType>* ArgumentT<StringType>::Next()
-{
-    int nextIndex = m_index + 1;
-
-    if (nextIndex > 0 && nextIndex < static_cast<int>(m_argList.size()))
-    {
-        return m_argList[nextIndex];
-    }
-    else
-    {
-        return nullptr;
-    }
-}
-
-template<class StringType>
-ArgumentT<StringType>* ArgumentT<StringType>::Previous()
-{
-    int prevIndex = m_index - 1;
-
-    if (prevIndex > 0 && prevIndex < static_cast<int>(m_argList.size()))
-    {
-        return m_argList[prevIndex];
-    }
-    else
-    {
-        return nullptr;
-    }
-}
-
-template<class StringType>
-typename const ArgumentT<StringType>::CharType* ArgumentT<StringType>::Text()
-{
-    return m_text.c_str();
-}
-
-template<class StringType>
-bool ArgumentT<StringType>::IsEqualTo(typename const CharType* text)
-{
-    return (0 == m_text.compare(text));
-}
 
 typedef ArgumentT<std::string> Argument;
 typedef ArgumentT<std::wstring> ArgumentW;
--- source/stl/arguments_parser.h
+++ source/stl/arguments_parser.h
@@ -7,75 +7,41 @@ class ArgumentsParserT
 {
 public:
     typedef typename StringType::value_type CharType;
+    typedef ArgumentT<StringType> ArgumentType;
 
-    ArgumentsParserT(int argc, CharType** argv);
-    virtual ~ArgumentsParserT();
+    ArgumentsParserT(int argc, CharType** argv)
+    {
+        if (argc > 0)
+        {
+            m_argList.reserve(argc);
+            for (int i = 0; i < argc; ++i)
+            {
+                m_argList.emplace_back(ArgumentType(m_argList, i, argv[i]));
+            }
+        }
+    }
 
-    bool Has(const CharType* text);
-    ArgumentT<StringType>* Get(const CharType* text);
+    bool Has(const CharType* text)
+    {
+        return (Get(text) != nullptr);
+    }
+
+    ArgumentType* Get(const CharType* text)
+    {
+        for (auto it = m_argList.begin(); it != m_argList.end(); ++it)
+        {
+            if ((*it).IsEqualTo(text))
+            {
+                return &(*it);
+            }
+        }
+
+        return nullptr;
+    }
 
 protected:
-    typename ArgumentT<StringType>::List m_argList;
+    typename ArgumentType::List m_argList;
 };
-
-template<class StringType>
-ArgumentsParserT<StringType>::ArgumentsParserT(int argc, typename CharType** argv)
-{
-    if (argc > 0)
-    {
-        m_argList.reserve(argc);
-    }
-
-    for (int i = 0; i < argc; ++i)
-    {
-        m_argList.push_back(new ArgumentT<StringType>(m_argList, i, argv[i]));
-    }
-}
-
-template<class StringType>
-ArgumentsParserT<StringType>::~ArgumentsParserT()
-{
-    ArgumentT<StringType>::List::iterator it = m_argList.begin();
-    while (it != m_argList.end())
-    {
-        delete (*it);
-        ++it;
-    }
-
-    m_argList.clear();
-}
-
-template<class StringType>
-bool ArgumentsParserT<StringType>::Has(typename const CharType* text)
-{
-    ArgumentT<StringType>::List::iterator it = m_argList.begin();
-    while (it != m_argList.end())
-    {
-        if ((*it)->IsEqualTo(text))
-        {
-            return true;
-        }
-        ++it;
-    }
-
-    return false;
-}
-
-template<class StringType>
-ArgumentT<StringType>* ArgumentsParserT<StringType>::Get(typename const CharType* text)
-{
-    Argument::List::iterator it = m_argList.begin();
-    while (it != m_argList.end())
-    {
-        if ((*it)->IsEqualTo(text))
-        {
-            return (*it);
-        }
-        ++it;
-    }
-
-    return nullptr;
-}
 
 typedef ArgumentsParserT<std::string> ArgumentsParser;
 typedef ArgumentsParserT<std::wstring> ArgumentsParserW;
